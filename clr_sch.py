245baa130d08291c4172c2fcffbde3b1f94e7e3d
Pratham <pratham10600@gmail.com>
2025-02-25 21:35:47 +0530

    color scheme

---
+++ clr_sch.py
@@ -0,0 +1,12 @@
+import cv2 
+img=cv2.imread("r1.jpg",1)
+cv2.imshow("image1",img)
+
+# colour scheme changing
+# img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# cv2.imshow("gray",img2)
+
+img3=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+cv2.imshow("rgb",img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
